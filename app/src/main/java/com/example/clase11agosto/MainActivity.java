131965f2e8a640d2f373e0b5892febee67fe773b
copito.123 <nicolspnicolsp@gmail.com>
2020-08-11 22:17:49 -0400

    hola cambios2

--- app/src/main/java/com/example/clase11agosto/MainActivity.java
+++ app/src/main/java/com/example/clase11agosto/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.clase11agosto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text);
+        Button button = findViewById(R.id.button);
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        final Toasty toasty = new Toasty(MainActivity.this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toasty.primaryToasty(v.getContext(), "3 missed calls", Toasty.LENGTH_LONG, Toasty.BOTTOM);
+            }
+        });
+
     }
 }
