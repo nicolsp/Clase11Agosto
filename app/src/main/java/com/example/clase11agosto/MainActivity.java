f2186ad152b2e9f53149585b4b8ef4874b86ff03
copito.123 <nicolspnicolsp@gmail.com>
2020-08-12 20:41:54 -0400

    hola cambios2

--- app/src/main/java/com/example/clase11agosto/MainActivity.java
+++ app/src/main/java/com/example/clase11agosto/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.clase11agosto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.prathameshmore.toastylibrary.Toasty;
 
 import timber.log.Timber;
 
@@ -14,20 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.text);
-        Button button = findViewById(R.id.button);
+
+        final Toasty toasty = new Toasty(this);
+
+
+        /*TextView textView = findViewById(R.id.text);
+
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
 
         final Toasty toasty = new Toasty(MainActivity.this);
-
+*/
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                toasty.primaryToasty(v.getContext(), "3 missed calls", Toasty.LENGTH_LONG, Toasty.BOTTOM);
+            public void onClick(View view) {
+                toasty.infoToasty(view.getContext(),"Holami nombre es nicols",Toasty.LENGTH_LONG,Toasty.BOTTOM,);
             }
-        });
-
+        }
     }
 }
+
+
+ 
